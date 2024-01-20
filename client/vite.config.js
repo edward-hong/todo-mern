7ac3a858720bd1a1d1358af0e920982c092097b0
edward-hong <edward.hong527@gmail.com>
2024-01-21 02:06:58 +1300

    relative vite build assets url

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 3000,
+    base: '',
     proxy: {
       '/auth': {
         target: 'http://localhost:5000',
